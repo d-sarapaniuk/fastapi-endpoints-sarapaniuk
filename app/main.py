6d3d46b49e42142256c1bee250e20934506e387f
d-sarapaniuk <d.sarapaniuk@gmail.com>
2026-03-22 11:50:58 +0200

    add logging

--- app/main.py
+++ app/main.py
@@ -1,8 +1,10 @@
+import logging
 from fastapi import FastAPI
 from app.utils import calculate_sum, repeat_text
 from app.schemas import SumRequest, RepeatRequest
 
 app = FastAPI()
+logging.basicConfig(level=logging.INFO)
 
 
 @app.get("/")
@@ -12,14 +14,17 @@ def root():
 
 @app.get("/health")
 def health():
+    logging.info("Healthcheck called")
     return {"status": "ok"}
 
 
 @app.post("/sum")
 def sum_values(payload: SumRequest):
+    logging.info(f"Calculating sum: {payload.a} + {payload.b}")
     return {"result": calculate_sum(payload.a, payload.b)}
 
 
 @app.post("/repeat")
 def repeat_value(payload: RepeatRequest):
+    logging.info(f"Repeating text '{payload.text}' {payload.times} times")
     return {"result": repeat_text(payload.text, payload.times)}
